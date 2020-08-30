a0eb93b3af9e9fd13ea4c7767160a405418b94e2
jimbobding <jimmygrahamdavid@gmail.com>
2020-08-30 22:26:40 +0000

    Added drinks page and cretaed base template

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for, flash, session
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+ 
+app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+mongo = PyMongo(app)
+
+
+# ---- Collection Variables ----- #
+
+drinks_db = mongo.db.drinks
 
 @app.route('/')
-def hello():
-    return "Hello world.......again"
-
+@app.route('/drinks')
+def drinks_card():
+    drinks = drinks_db.find()
+    return render_template("drinks.html", drinks=drinks)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
