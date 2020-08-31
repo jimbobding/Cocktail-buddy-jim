d348b02c2f210246b034db54fdec5af8e6a85053
jimbobding <jimmygrahamdavid@gmail.com>
2020-08-31 12:01:14 +0000

    Added session user profile

--- app.py
+++ app.py
@@ -49,11 +49,12 @@ def register():
             "password": generate_password_hash(request.form.get("password").lower())
         }
 
-        mongo.db.users.insert_one(register)
+        users_db.insert_one(register)
 
         #put user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Succesfull")
+        return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
 
@@ -70,7 +71,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
 
             else:
                 # invalid match 
@@ -80,8 +84,16 @@ def login():
         else: 
             flash("Incorect password and/or username")
             return redirect(url_for("login")) 
-             
+
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=['GET', 'POST'])
+def profile(username):
+    # gets session username from the database
+    username = users_db.find_one(
+       {"username": session["user"]}) ["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == '__main__':
